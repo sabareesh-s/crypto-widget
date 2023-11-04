6f378ea6662e4f66304197e21164c8b7fa623c5d
sabareesh-s <sabareesh1221@gmail.com>
2023-11-04 16:11:28 +0530

    styling changes

--- coinGeckoWidget.js
+++ coinGeckoWidget.js
@@ -198,14 +198,14 @@
                                     transform: ${priceChange < 0 ? 'scaleY(-1)' : ''};
                                     " 
 
-                        class="icon-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="${priceChange < 0 ? '#b91c1c' : '#15803d'}">
+                        class="icon-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="${priceChange < 0 ? '#ef4444' : '#65a30d'}">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                         </svg>
                   
 
                         <div class="price-change">
                             <p>PRICE</p>
-                            <span style="color: ${priceChange < 0 ? '#b91c1c' : '#15803d'}">₹${priceChange}</span>
+                            <span style="color: ${priceChange < 0 ? '#ef4444' : '#65a30d'}">₹${priceChange}</span>
                         </div>
                     </div>
 
